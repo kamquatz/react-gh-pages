9a1700408ebecf4ac1bf603a5c2acac0e8af2671
kamquatz <dennismuga@gmail.com>
2019-06-26 14:04:38 +0300

    Updates

---
+++ precache-manifest.6ba3ba4c6c7c73a60b19e0b00d73c915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e928823ea5a925b89f574c8da784ab3c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2b8f7a6d443a4c4da5f9",
+    "url": "/react-gh-pages/static/css/main.59284a20.chunk.css"
+  },
+  {
+    "revision": "8b4708b77cca9b3e09bb",
+    "url": "/react-gh-pages/static/js/2.2ea8e94e.chunk.js"
+  },
+  {
+    "revision": "2b8f7a6d443a4c4da5f9",
+    "url": "/react-gh-pages/static/js/main.66d8734b.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.cae92349879c43c936844ca6b9055730.js"
+  "/react-gh-pages/precache-manifest.6ba3ba4c6c7c73a60b19e0b00d73c915.js"
 );
 
 self.addEventListener('message', (event) => {
